e934fb8c201e0017da7776ecb942d4c6162fc172
hugo emannuel <hugoemannuel53@gmail.com>
2025-08-21 21:01:35 -0300

    fix pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
 "use client";
-import { useQuery } from "react-query";
-import { queryKeys } from "./global/variables/queryKeys";
-import gamesService from "./service/gamesService";
 
 export default function Home() {
-  const { data } = useQuery({
-    queryKey: [queryKeys.games.list],
-    queryFn: () => gamesService.allGames(),
-  });
-
-  console.log(data);
-
   return <h1>Home</h1>;
 }
